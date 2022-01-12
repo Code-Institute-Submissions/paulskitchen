c1a2a13d2005041950a9d200a65c94b2d4ea9764
PaulModaley <pmodaley@gmail.com>
2022-01-12 16:38:50 +0000

    add models to admin.py file, including Booking and UserProfile

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Booking, UserProfile
 
 # Register your models here.
+admin.site.site_header = 'Pauls Kitchen Dashboard'
+
+admin.site.register(Booking)
+admin.site.register(UserProfile)
+
